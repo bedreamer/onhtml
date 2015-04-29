1bbe5724a56d1ca65d608a4933547caae4646cbb
jerry <jerry@94b0928a-2210-914b-9fec-d676ee53cfae>
2015-04-29 04:52:02 +0000

    git-svn-id: svn://192.168.1.8/html/trunk@102 94b0928a-2210-914b-9fec-d676ee53cfae

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 printf "fetching file.list...."
-tftp -g -r $FILELIST /tmp/$FILELIST $1 >/dev/null 2>&1
+tftp -g -r $FILELIST -l /tmp/$FILELIST $1 >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "          [ DONE]"
 else
@@ -17,7 +17,7 @@ fi
 
 for f in `cat /tmp/$FILELIST`; do
 	printf "get $f from $1"
-	tftp -g -r $f $1 >/dev/null 2>&1
+	tftp -g -r $f -l $f $1 >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "          [ DONE]"
 	else
